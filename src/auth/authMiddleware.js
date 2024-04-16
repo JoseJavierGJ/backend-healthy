bfd599b89a9ddadce03fde372a8d7c4fc6597745
JoseJavierGJ <jjosejavier@hotmail.com>
2024-04-16 13:58:00 -0600

    Correciones

--- src/auth/authMiddleware.js
+++ src/auth/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const authenticateToken = (req, res, next) => {
-  const token = req.headers['authotization']
+  const token = req.headers['authorization']
   const access = token.split(' ')
   console.log('@@ token => ', access[1])
   if (!access[1]) {
@@ -11,7 +11,7 @@ const authenticateToken = (req, res, next) => {
     })
   }
 
-  jwt.verify(token, process.env.SECRET, (err, user) => {
+  jwt.verify(access[1], process.env.SECRET, (err, user) => {
     if (err) {
       return res.status(403).json({
         message: 'Forbidden'
